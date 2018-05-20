2b1adbec1232e1a47ee12a09f5696f1188e09481
irhode <rhode@gmx.es>
2018-05-20 12:23:05 +0200

    Modular project

---
+++ spring3/rest-spring3/src/main/java/io/edu/spring/App.java
@@ -0,0 +1,13 @@
+package io.edu.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
